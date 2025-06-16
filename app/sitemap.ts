35a81f805647f0f7f12183a41f888b153ada548a
Xinyi Jiang <xinyij@google.com>
2025-06-15 20:04:59 -0700

    Fix sitemap issues.
    Hopefully fixes Github Actions

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,37 +1,36 @@
-import { promises as fs } from 'fs';
-import path from 'path';
+import { promises as fs } from "fs";
+import path from "path";
 
-const SITE_URL = 'https://next-mdx-blog.vercel.app';
+export const dynamic = "force-static";
+
+const SITE_URL = "https://next-mdx-blog.vercel.app";
 
 async function getNoteSlugs(dir: string) {
   const entries = await fs.readdir(dir, {
     recursive: true,
-    withFileTypes: true
+    withFileTypes: true,
   });
   return entries
-    .filter((entry) => entry.isFile() && entry.name === 'page.mdx')
+    .filter((entry) => entry.isFile() && entry.name === "page.mdx")
     .map((entry) => {
-      const relativePath = path.relative(
-        dir,
-        path.join(entry.parentPath, entry.name)
-      );
-      return path.dirname(relativePath);
+      const filePathRelativeToDir = path.join(entry.path, entry.name);
+      return path.dirname(filePathRelativeToDir);
     })
-    .map((slug) => slug.replace(/\\/g, '/'));
+    .map((slug) => slug.replace(/\\/g, "/"));
 }
 
 export default async function sitemap() {
-  const notesDirectory = path.join(process.cwd(), 'app', 'n');
+  const notesDirectory = path.join(process.cwd(), "app", "2025");
   const slugs = await getNoteSlugs(notesDirectory);
 
   const notes = slugs.map((slug) => ({
-    url: `${SITE_URL}/n/${slug}`,
-    lastModified: new Date().toISOString()
+    url: `${SITE_URL}/2025/${slug}`,
+    lastModified: new Date().toISOString(),
   }));
 
-  const routes = ['', '/work'].map((route) => ({
+  const routes = ["", "/work"].map((route) => ({
     url: `${SITE_URL}${route}`,
-    lastModified: new Date().toISOString()
+    lastModified: new Date().toISOString(),
   }));
 
   return [...routes, ...notes];
